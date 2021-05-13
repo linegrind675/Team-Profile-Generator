626603328537e038a9882e7610c9d8368162dd36
Linegrind <linegrind@icloud.com>
2021-05-12 20:36:07 -0600

    intern generator and functionality

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -36,3 +36,22 @@ const generateEngineer = function (engineer) {
     `;
 }
 
+const generateIntern = function(intern) {
+    return `
+    <div class="col-4 mt-4">
+                    <div class="card">
+                        <div class="card header">
+                            <h3>${intern.name}</h3>
+                            <h4>Intern</h4>
+                        </div>
+
+                        <div class="card-body">
+                            <p class="id">ID: ${intern.id}</p>
+                            <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                            <p class="school">School: ${intern.school}</p>
+                        </div>
+                    </div>
+                </div>
+    `;
+}
+
