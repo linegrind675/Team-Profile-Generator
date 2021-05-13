aa25572fe3e634da3329f9bca5505a643e47b3ae
Linegrind <linegrind@icloud.com>
2021-05-12 20:32:49 -0600

    generate an engineer

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,4 +1,4 @@
-const generateManager = function(manager) {
+const generateManager = function (manager) {
     return `
     <div class="col-4 mt-4">
                     <div class="card">
@@ -11,6 +11,25 @@ const generateManager = function(manager) {
                             <p class="id">ID: ${manager.id}</p>
                             <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                             <p class="office">Office Number: ${manager.officeNumber}</p>
+                        </div>
+                    </div>
+                </div>
+    `;
+}
+
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+                    <div class="card">
+                        <div class="card header">
+                            <h3>${engineer.name}</h3>
+                            <h4>Engineer</h4>
+                        </div>
+
+                        <div class="card-body">
+                            <p class="id">ID: ${engineer.id}</p>
+                            <p class="email">Email: <a href="${engineer.email}">${engineer.email}</a></p>
+                            <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
                         </div>
                     </div>
                 </div>
